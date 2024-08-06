144911dc7b8f2c6b8dde1ffbee350aada67702ee
Gahyung Kim <ghkim10202@gmail.com>
2024-08-06 23:40:36 +0900

    부분합 문제 풀이

--- 1.누적합/p.1806.py
+++ 1.누적합/p.1806.py
@@ -4,22 +4,33 @@
 URL: https://www.acmicpc.net/problem/1806
 '''
 
-# import sys
-# input = sys.stdin.readline
-# n,S = map(int, input().split())
-# numbers = [0]
-# numbers += list(map(int, input().split()))
-n, S = 10, 15
-numbers = [5, 1, 3, 5, 10, 7, 4, 9, 2, 8]
+# 기본 탐색 방법으로는 시간초과가 일어난다
+# 따라서 투포인터를 사용해서 풀이해야 한다.
 
-result = 1000
-for i in range(n): 
-    tmp = [numbers[i]]
-    for j in range(i, n):
-        tmp.append(tmp[j-1] + numbers[j])
-        if tmp[j] >= S:
-            if len(tmp) - 1 < result:
-                result = len(tmp) - 1
-                break
-                
-print(result)
+import sys
+input = sys.stdin.readline
+# n,S = map(int, input().split())
+# numbers = list(map(int, input().split()))
+n,S = 10, 15
+numbers = [5, 1, 3, 5, 10, 7, 4, 9, 2, 8]
+left, right = 0,0
+sum_value = 0
+min_length = sys.maxsize
+
+while True:
+    # 만약 총 합이 S가 넘는다면, left를 하나씩 옮겨보면서 어디까지 길이가 줄어드나 확인
+    if sum_value >=S:
+        min_length = min(min_length, right - left)
+        sum_value-=numbers[left]
+        left+=1
+    elif right == n:
+        break
+    # 만약 총합이 S를 넘지 않는다면 right을 오른쪽으로 한칸씩 옮기며 총합이 S를 넘을 때 까지 더함
+    else:
+        sum_value +=numbers[right]
+        right+=1
+
+if min_length == sys.maxsize:
+    print(0)
+else:
+    print(min_length)

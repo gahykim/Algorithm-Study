6a50c6503d48554df69de9a97457c4c2572f5087
Gahyung Kim <ghkim10202@gmail.com>
2024-08-09 17:04:36 +0900

    2559번 수열 문제 풀이

--- p.2559.py
+++ p.2559.py
@@ -3,3 +3,17 @@
    
 URL: https://www.acmicpc.net/problem/2559
 '''
+import sys
+
+N, K = map(int, sys.stdin.readline().split())
+numbers = list(map(int, sys.stdin.readline().split()))
+
+sum_value = sum(numbers[:K])
+result = sum_value
+
+for i in range(N-K): 
+    new_sum_value = sum_value - numbers[i] + numbers[i+K]
+    if new_sum_value > result:
+        result = new_sum_value
+    sum_value = new_sum_value
+print(result)
